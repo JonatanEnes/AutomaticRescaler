e361d1d9b524cdcd00c6c00500200385ae31b84c
jonatan <jonatan.enes@udc.es>
2018-11-04 16:27:24 +0100

    Remove tests so that CI is not applied

--- set_pythonpath.fish
+++ set_pythonpath.fish
@@ -1,5 +1,5 @@
 #!/usr/bin/fish
-set -g DEV_PATH $HOME/development
+set -g DEV_PATH $HOME/production
 set -g RESCALER_PATH $DEV_PATH/automatic-rescaler
 set -g BDWACHDOG_PATH $DEV_PATH/metrics-to-time-series
 set -g APPLICATION_TIMESTAMPS_PATH $DEV_PATH/applications-timestamps-snitch
--- set_pythonpath.sh
+++ set_pythonpath.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export DEV_PATH=$HOME/development
+export DEV_PATH=$HOME/production
 export RESCALER_PATH=$DEV_PATH/automatic-rescaler
 export BDWACHDOG_PATH=$DEV_PATH/metrics-to-time-series
 export APPLICATION_TIMESTAMPS_PATH=$DEV_PATH/applications-timestamps-snitch
